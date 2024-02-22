f2eb5af3dfc89ac8645e609b9991e07431c7769d
KeKelv38 <k.grimault@rns.ecolescreatives.com>
2024-02-22 16:56:39 +0100

    sauvergarde_jeudi_22_02
    dernier cours de prog

--- Assets/Scripts/AttackHitBox.cs
+++ Assets/Scripts/AttackHitBox.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class AttackHitBox : MonoBehaviour
 {
+    private bool isAttacking;
     public Animator animator;
+    private float timer = 0f;
+    private float timeToAttack = 0.25f;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        isAttacking = false;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            isAttacking = true;
             animator.Play("Attack");
+        }
+
+        if (isAttacking)
+        {
+            timer += Time.deltaTime;
+
+            if(timer >= timeToAttack)
+            {
+                timer = 0;
+                isAttacking = false;
+            }
         }
     }
 }
